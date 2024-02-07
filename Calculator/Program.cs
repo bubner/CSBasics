b496294357f6d0ec2f4872ba89a8e8a9ac0f3a79
Lucas Bubner <lkbubner@proton.me>
2024-02-07 17:32:43 +1030

    Initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    private static readonly char[] Operations = ['+', '-', '*', '/'];
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter expression to evaluate (left to right):");
+        string? userInput;
+        for (;;)
+        {
+            // Repeatedly ask until we get a non-empty and usable string
+            userInput = Console.ReadLine();
+            if (!String.IsNullOrEmpty(userInput))
+            {
+                break;
+            }
+            Console.WriteLine("Invalid!");
+        }
+
+        // Convert strings to doubles
+        double[] nums = ConvertToDoubleArray(userInput.Split(Operations));
+
+        // Extract operators from the string
+        ArrayList ops = [];
+        foreach (char c in userInput)
+        {
+            if (Operations.Contains(c))
+            {
+                ops.Add(c);
+            }
+        }
+
+        // Perform calculations
+        double res = 0;
+        foreach (double d in nums)
+        {
+            if (res == 0)
+            {
+                // The first number will not have an operator as it is the start of the expression
+                // so we will need to set the result to the first number
+                res = d;
+                continue;
+            }
+            // Queue up the next operator
+            char currentOperator = (char) ops[0];
+            ops.RemoveAt(0);
+            // Perform the operation
+            switch (currentOperator)
+            {
+                case '+':
+                    res += d;
+                    break;
+                case '-':
+                    res -= d;
+                    break;
+                case '*':
+                    res *= d;
+                    break;
+                case '/':
+                    res /= d;
+                    break;
+            }
+        }
+        Console.WriteLine(res);
+    }
+
+    /// <summary>
+    /// Convert a string array to a double array
+    /// </summary>
+    /// <param name="strings">an array of strings</param>
+    /// <returns>an array of doubles</returns>
+    /// <exception cref="ArgumentException">if a cast was unsuccessful</exception>
+    private static double[] ConvertToDoubleArray(string[] strings)
+    {
+        double[] res = new double[strings.Length];
+        for (int i = 0; i < strings.Length; i++)
+        {
+            if (double.TryParse(strings[i], out double result))
+            {
+                res[i] = result;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid expression: " + strings[i]);
+            }
+        }
+        return res;
+    }
+}
---
+++ QuizPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace QuizPractice;
+
+class Program
+{
+    private static Dictionary<string, string> _qa  = new Dictionary<string, string>
+    {
+        {"What is 1+1?", "2"},
+        {"What is my name?", "Lucas Bubner"},
+        {"Where is Earth?", "Solar system"},
+        {"Can you eat a cactus?", "yes"}
+    };
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to one of the quizzes of all time!");
+        Console.WriteLine("Press any key to start...");
+        Console.ReadKey();
+        Console.Clear();
+        int correctQuestions = 0;
+        foreach (KeyValuePair<string, string> entry in _qa)
+        {
+            Console.WriteLine(entry.Key);
+
+            string? userInput;
+            for (;;)
+            {
+                userInput = Console.ReadLine();
+                if (!String.IsNullOrEmpty(userInput))
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid!");
+            }
+
+            if (userInput.ToLower() == entry.Value.ToLower())
+            {
+                Console.WriteLine("Correct!");
+                correctQuestions++;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect!");
+            }
+        }
+        Console.WriteLine($"You got {correctQuestions}/{_qa.Count} questions right!");
+        if (correctQuestions == _qa.Count)
+        {
+            Console.WriteLine("You are a genius!");
+        }
+
+        Console.ReadKey();
+    }
+}
